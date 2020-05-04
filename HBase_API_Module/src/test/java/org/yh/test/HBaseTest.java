7043e19973b55428f697675d55c8f0f0d916873c
Dylan <18228869131@163.com>
2020-05-04 12:16:44 +0800

    Update HBase_API_Module module date: Monday, May, 4, 2020

---
+++ HBase_API_Module/src/test/java/org/yh/test/HBaseTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2020 The YuanHao Authors
+ * File name: HBaseTest.java
+ * File type: JAVA
+ * Create time: 5/4/2020 11:42 AM
+ * Address: Chengdu, China
+ * Email: 18228869131@163.com
+ */
+package org.yh.test;
+import org.junit.Test;
+
+import java.io.Serializable;
+
+/**
+ * @className HBaseTest
+ * @author YUAN_HAO
+ * @time 5/4/2020 11:42 AM
+ * @category 
+ * @description  
+ * @version 1.0
+ */
+public class HBaseTest implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Test
+    public void Test() {
+        System.out.println("Today is a sunny weather, I'm so happy.\n");
+    }
+
+        
+}
